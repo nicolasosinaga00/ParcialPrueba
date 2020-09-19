7b296627396f166582af41e3cc84d8334e27bd2a
nicolasosinaga00 <a2019114771@estudiantes.upsa.edu.bo>
2020-09-19 10:12:38 -0400

    Con controlador y todo

---
+++ ParcialPrueba/ApiWeb/Models/Country.cs
@@ -0,0 +1,50 @@
+﻿namespace ApiWeb.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    public class Currency
+    {
+        [Key]
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+    }
+    public class GeoRef
+    {
+        [Key]
+        [Required]
+        [Range(-90, 90)]
+        public double Latitude { get; set; }
+        [Required]
+        [Range(-180, 180)]
+        public double Longitude { get; set; }
+
+    }
+    public class Country
+    {
+        [Key]
+        [Required(ErrorMessage = "You must enter the field{0}")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field{0}")]
+        public string capital { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field{0}")]
+        public int population { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field{0}")]
+        public List<GeoRef> lating { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field{0}")]
+        public List<String>  timezones { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field{0}")]
+        public List<Currency> currencies { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field{0}")]
+        [Url]
+        public string flag { get; set; }
+
+    }
+}
---
+++ ParcialPrueba/ApiWeb/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApiWeb.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ApiWeb.Models.Country> Countries { get; set; }
+    }
+}
